9f60f3606e3bd08a426ba60e5bad382cc0449ece
notneerajsah <notneerajkumarsah@gmail.com>
2026-04-04 00:37:57 +0530

    server url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
 // export const ServerUrl  = "http://localhost:8000"
-export const ServerUrl  = "https://your-backend.onrender.com/api/user/current-user"
+export const ServerUrl  = "https://hiremindai.onrender.com"
 function App() {
 
   const dispatch = useDispatch()
